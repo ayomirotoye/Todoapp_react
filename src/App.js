8d71b7bd6993ad31c031e1206de1ffe01c91413a
Ayomirotoye <ayomirotoye@gmail.com>
2020-02-21 10:26:10 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario game'}
+    ]
+  }
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
